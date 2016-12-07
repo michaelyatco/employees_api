1959cf74acfafaa7ffd531adb24d8f8ede7ca202
Peter Jang <peter.x.jang@gmail.com>
2016-12-06 19:50:12 -0600

    Add employee create action with json

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,6 +4,19 @@ class EmployeesController < ApplicationController
     render 'index.json.jbuilder'
   end
 
+  def create
+    @employee = Employee.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      birthdate: params[:birthdate],
+      email: params[:email],
+      ssn: params[:ssn]
+    )
+    @employee.save
+    # redirect_to "/employees/#{@employee.id}"
+    render 'show.json.jbuilder'
+  end
+
   def show
     @employee = Employee.find_by(id: params[:id])
     render 'show.json.jbuilder'
