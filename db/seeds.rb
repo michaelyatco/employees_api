feb5afa092e0db8e98ea35d1ff1bd166daa33ac9
Peter Jang <peter.x.jang@gmail.com>
2016-12-22 18:56:06 -0600

    Uncomment seed lines for complete fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 100.times do
-#   first_name = Faker::Name.first_name
-#   last_name = Faker::Name.last_name
-#   Employee.create(
-#     first_name: first_name,
-#     last_name: last_name,
-#     email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
-#     ssn: Faker::Medical::SSN.ssn,
-#     birthdate: Faker::Date.between(18.years.ago, 65.years.ago)
-#   )
-# end
+100.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  Employee.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+    ssn: Faker::Medical::SSN.ssn,
+    birthdate: Faker::Date.between(18.years.ago, 65.years.ago)
+  )
+end
 
 200.times do
   Address.create(
